9060e6059b8f3e9493d9aea95003f0af520222ae
genarvaez <ge_naravez@hotmail.com>
2017-06-20 10:38:36 -0400

    ejercicio guiado terminado

---
+++ mains.js
@@ -0,0 +1,39 @@
+
+
+function initMap(){
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom:5,
+		center: {lat: -9.1191427, lng: -77.0349046},
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false,
+	});
+
+	function buscar(){
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(funcionExito, funcionError)
+		}
+	}
+
+	document.getElementById("found").addEventListener("click", buscar);
+	var longitud, latitud;
+
+	var funcionExito = function(posicion){
+		latitud = posicion.coords.latitude;
+		longitud = posicion.coords.longitude;
+	
+
+	var miUbicacion = new google.maps.Marker({
+		position: {lat: latitud, lng: longitud},
+		animation: google.maps.Animation.DROP, 
+		map: map
+	});
+
+	map.setZoom(17);
+	map.setCenter({lat: latitud, lng:longitud})
+	}
+
+	var funcionError = function(error){
+		alert("tenemos problemas para encontrar tu ubicacion")
+	}
+}
